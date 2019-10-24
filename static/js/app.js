449ef8c71f79cc59bba688169320b481e95dea2c
Nayanna Nara <nayanna501@gmail.com>
2019-10-24 18:34:35 -0300

    ajuste

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,4 @@
-/*$(function() {
+$(function() {
 
     $('.dates #datepicker').datepicker({
         'format': 'dd/mm/yyyy',
@@ -8,7 +8,7 @@
 
 
 });
-*/
+
 function onlynumber(evt) {
    var theEvent = evt || window.event;
    var key = theEvent.keyCode || theEvent.which;
@@ -23,8 +23,8 @@ function onlynumber(evt) {
     $(document).ready(function(){
     $('#celular').mask("(99) 99999-9999");
 });
-     
-     
+
+
 function mascara(t, mask){
      var i = t.value.length;
      var saida = mask.substring(1,0);
@@ -76,17 +76,17 @@ function validaCPF(cpf) {
     var Resto;
     Soma = 0;
   if (cpf == "00000000000") return false;
-     
+
   for (i=1; i<=9; i++) Soma = Soma + parseInt(cpf.substring(i-1, i)) * (11 - i);
   Resto = (Soma * 10) % 11;
-   
+
     if ((Resto == 10) || (Resto == 11))  Resto = 0;
     if (Resto != parseInt(cpf.substring(9, 10)) ) return false;
-   
+
   Soma = 0;
     for (i = 1; i <= 10; i++) Soma = Soma + parseInt(cpf.substring(i-1, i)) * (12 - i);
     Resto = (Soma * 10) % 11;
-   
+
     if ((Resto == 10) || (Resto == 11))  Resto = 0;
     if (Resto != parseInt(cpf.substring(10, 11) ) ) return false;
     return true;
